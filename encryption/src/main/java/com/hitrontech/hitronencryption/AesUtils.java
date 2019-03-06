ddd023816591422b06b1cb852692a2e5bb3e6953
Daniel Du <duqishuang@sz.hitrontech.com>
2019-03-06 14:47:38 +0800

    [New Project|Encryption]create the new project

---
+++ encryption/src/main/java/com/hitrontech/hitronencryption/AesUtils.java
@@ -0,0 +1,97 @@
+package com.hitrontech.hitronencryption;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by admin on 2017/6/6.
+ */
+
+public class AesUtils {
+    private final static String HEX = "0123456789ABCDEF";
+    private static final String CBC_PKCS5_PADDING = "AES/CBC/PKCS5Padding";//AES是加密方式 CBC是工作模式 PKCS5Padding是填充模式
+    private static final String AES = "AES";//AES 加密
+
+    // 对密钥进行处理
+    // https://android-developers.googleblog.com/2016/06/security-crypto-provider-deprecated-in.html
+    private static byte[] getRawKey(String seed) throws Exception {
+        byte[] arrBTmp = seed.getBytes();
+        byte[] arrB = new byte[16];
+        for (int i = 0; i < arrBTmp.length && i < arrB.length; i++) {
+            arrB[i] = arrBTmp[i];
+        }
+        SecretKeySpec skeySpec = new SecretKeySpec(arrB, AES);
+        return skeySpec.getEncoded();
+    }
+
+    /*
+     * 加密
+     */
+    static String encrypt(String key, String cleartext) {
+        if (TextUtils.isEmpty(cleartext)) {
+            return cleartext;
+        }
+        try {
+            byte[] result = encrypt(key, cleartext.getBytes());
+            return Base64Encoder.encode(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*
+    * 加密
+    */
+    private static byte[] encrypt(String key, byte[] clear) throws Exception {
+        byte[] raw = getRawKey(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, AES);
+        Cipher cipher = Cipher.getInstance(CBC_PKCS5_PADDING);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+        byte[] encrypted = cipher.doFinal(clear);
+        return encrypted;
+    }
+
+    static String decrypt(String key, String encrypted) {
+        if (TextUtils.isEmpty(encrypted)) {
+            return encrypted;
+        }
+        try {
+            byte[] enc = Base64Decoder.decodeToBytes(encrypted);
+            Log.i("123", "===>>enc  " + enc);
+            byte[] result = decrypt(key, enc);
+            return new String(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static byte[] decrypt(String key, byte[] encrypted) throws Exception {
+        byte[] raw = getRawKey(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, AES);
+        Cipher cipher = Cipher.getInstance(CBC_PKCS5_PADDING);
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+        byte[] decrypted = cipher.doFinal(encrypted);
+        return decrypted;
+    }
+
+    //二进制转字符
+    static String toHex(byte[] buf) {
+        if (buf == null)
+            return "";
+        StringBuffer result = new StringBuffer(2 * buf.length);
+        for (int i = 0; i < buf.length; i++) {
+            appendHex(result, buf[i]);
+        }
+        return result.toString();
+    }
+
+    private static void appendHex(StringBuffer sb, byte b) {
+        sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
+    }
+}
---
+++ encryption/src/main/java/com/hitrontech/hitronencryption/EncryptionManager.java
@@ -0,0 +1,80 @@
+package com.hitrontech.hitronencryption;
+
+import android.content.Context;
+import android.util.Log;
+
+public class EncryptionManager implements EncryptionImpl{
+    private String mTag = "Hitron:" + getClass().getSimpleName();
+
+    private EncryptionManager() {
+
+    }
+
+    public static EncryptionImpl getInstance() {
+        return EncryptionManagerHolder.sInstance;
+    }
+
+    private static class EncryptionManagerHolder {
+        private static EncryptionImpl sInstance = new EncryptionManager();
+    }
+
+    @Override
+    public String base64Encoder(String key, String text) {
+        return AesUtils.encrypt(key, text);
+    }
+
+    @Override
+    public String base64Encoder(Context context, String key, String text) {
+        String packageName = null;
+        if (context == null) {
+            Log.w(mTag, "Context is NULL");
+        } else {
+            packageName = context.getPackageName();
+        }
+        return base64EncoderByAppId(packageName, key, text);
+    }
+
+    @Override
+    public String base64EncoderByAppId(String applicationId, String key, String text) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(applicationId);
+        sb.append(key);
+        String securityKey = sb.toString();
+        return base64Encoder(securityKey, text);
+    }
+
+    @Override
+    public String base64Decoder(String key, String encoderStr) {
+        return AesUtils.decrypt(key, encoderStr);
+    }
+
+    @Override
+    public String base64Decoder(Context context, String key, String encoderStr) {
+        String packageName = null;
+        if (context == null) {
+            Log.w(mTag, "Context is NULL");
+        } else {
+            packageName = context.getPackageName();
+        }
+        return base64DecoderByAppId(packageName, key, encoderStr);
+    }
+
+    @Override
+    public String base64DecoderByAppId(String applicationId, String key, String encoderStr) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(applicationId);
+        sb.append(key);
+        String securityKey = sb.toString();
+        return base64Decoder(securityKey, encoderStr);
+    }
+
+    @Override
+    public String md5Str(String text) {
+        return Md5Utils.md5Str(text);
+    }
+
+    @Override
+    public String md5File(String filePath) {
+        return Md5Utils.getFileMd5(filePath);
+    }
+}
